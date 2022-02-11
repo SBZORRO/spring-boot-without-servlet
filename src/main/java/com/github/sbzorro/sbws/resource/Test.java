2f5f6481e3294d3a593c71f0e0cc852933a60238
SBZORRO <SBZORRO@users.noreply.github.com>
2022-02-11 19:20:45 +0800

    Update Test.java

--- src/main/java/com/github/sbzorro/sbws/resource/Test.java
+++ src/main/java/com/github/sbzorro/sbws/resource/Test.java
@@ -12,9 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 @Component
 public class Test {
 
-    /**
-     * 根据用户名称获取用户详情
-     */
 //	@GetMapping("/test")
 	@GET
 	@Path("/test")
